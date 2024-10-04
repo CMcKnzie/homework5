49812937f17ecb569c3022d88f6373359b09954f
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-04 18:40:06 -0500

    page nav working

---
+++ src/components/MainPaigination.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const pages = ref(10)
+const activePage = ref(5)
+
+const prev = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+        console.log(activePage.value)
+    }
+}
+
+const next = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+        console.log(activePage.value)
+    }
+}
+
+
+</script>
+
+<template>
+    <div>
+        <button @click="prev" :disabled="activePage === 1">PREV</button>
+
+        <button v-for="page in pages" :key="page" class="">{{ page }}</button>
+
+        <button @click ="next" :disable="activePage ===pages" >NEXT</button>
+    </div>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,12 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 
-import MainCards from '@/components/MainCards.vue';
+import MainCards from '@/components/MainCards.vue'
+import MainPaigination from '@/components/MainPaigination.vue';
 
 </script>
 
 <template>
   <MainCards></MainCards>
+  <MainPaigination></MainPaigination>
 </template>
