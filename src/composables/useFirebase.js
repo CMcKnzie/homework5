d369f1fe69db30e41e13fdae4027dfcf4112940c
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-04 16:02:02 -0500

    added firebase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwn16E6abKbpcPo1-u59mY2DRic5Y-9EE",
+  authDomain: "comapnyweb.firebaseapp.com",
+  projectId: "comapnyweb",
+  storageBucket: "comapnyweb.appspot.com",
+  messagingSenderId: "860286491906",
+  appId: "1:860286491906:web:c717518d30025ce630b61b"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
